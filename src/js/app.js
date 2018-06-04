eab48544d03df6cfab920e764dd3e87098035e70
hazymoon <imiku.me@gmail.com>
2018-06-04 18:07:17 +0800

    首屏布局完成

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,29 @@
+/*
+ * @Author: Ice-Hazymoon 
+ * @Date: 2018-06-04 15:41:56 
+ * @Last Modified by: Ice-Hazymoon
+ * @Last Modified time: 2018-06-04 16:59:35
+ */
 (function(){
     function hitoko(){
-        let h = '一言: 我们所度过的每个平凡的日常，也许就是连续发生的奇迹';
-        let dom = document.getElementById('hitokoto')
-        fetch('https://v1.hitokoto.cn/?c=a').then(response=>{
+        let h = '—— Hitoko: 我们所度过的每个平凡的日常，也许就是连续发生的奇迹';
+        let dom = document.getElementById('hitokoto');
+        fetch('https://v1.hitokoto.cn/?c=e').then(response=>{
             return response.json();
         }).then(data=>{
-            let text = data.hitokoto.length > 55 ? h : '一言: ' + data.hitokoto;
+            let text = data.hitokoto.length > 55 ? h : '—— Hitoko: ' + data.hitokoto;
             dom.textContent = text;
         }).catch(e=>{
-            console.log('一言获取失败： '+e)
+            console.log('一言获取失败： '+e);
             dom.textContent = h;
         });
     };hitoko();
+
+    document.getElementById('refresh-hitokoto').addEventListener('click', function(e){
+        hitoko();
+        this.classList.add('re');
+        this.addEventListener('transitionend', function(e){
+            this.classList.remove('re');
+        })
+    })
 }())
